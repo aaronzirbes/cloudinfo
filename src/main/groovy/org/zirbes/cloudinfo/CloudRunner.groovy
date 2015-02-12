5bcff01943875b3813faddd421be8f611f1e21d5
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-02-11 22:26:18 -0600

    a start

---
+++ src/main/groovy/org/zirbes/cloudinfo/CloudRunner.groovy
@@ -0,0 +1,10 @@
+package org.zirbes.cloudinfo
+
+class CloudRunner {
+
+    static void main(String[] argv) {
+        log.info 'Initializinz cloudinfo'
+
+        log.info 'Exiting cloudinfo'
+    }
+}
